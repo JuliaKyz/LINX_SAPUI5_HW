9b8bd095ebd2803a3563c110abe2b332e6d05997
JuliaKyz <Julie.kz7777@gmail.com>
2024-11-21 23:37:34 +0300

    added lesson_10

--- OrionContent/Worklist/webapp/controller/Worklist.controller.js
+++ OrionContent/Worklist/webapp/controller/Worklist.controller.js
@@ -11,12 +11,257 @@ sap.ui.define([
 
     return BaseController.extend("zjblessons.Worklist.controller.Worklist", {
         formatter: formatter,
+        
+      jsonAnnotation: new JSONModel({
+      RequestAtLeast: "",
+      Annotation: [
+        {
+          "id": "HeaderID",
+          "label": "{i18n>HeaderID}",
+          "Column": {
+            "order": 1,
+            "sortProperty": "HeaderID",
+            "visible": true,
+            "type": "text",
+            "select": "HeaderID",
+            "text": "{HeaderID}"
+          },
+          "Filter": {
+            "order": 1,
+            "mode": "SingleSelectMaster",
+            "filter": "HeaderID",
+            "text": "HeaderID",
+            "sort": "HeaderID",
+            "key": "HeaderID",
+            "entitySet": "zjblessons_base_Headers",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "MaterialID",
+          "label": "{i18n>MaterialID}",
+          "Column": {
+            "order": 2,
+            "sortProperty": "MaterialID",
+            "visible": true,
+            "type": "text",
+            "select": "MaterialID",
+            "text": "{MaterialID}"
+          },
+          "Filter": {
+            "order": 2,
+            "mode": "MultiSelect",
+            "filter": "MaterialID",
+            "text": "MaterialID",
+            "sort": "MaterialID",
+            "key": "MaterialID",
+            "entitySet": "zjblessons_base_Materials",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "GroupID",
+          "label": "{i18n>GroupID}",
+          "Column": {
+            "order": 3,
+            "sortProperty": "GroupID",
+            "visible": true,
+            "type": "text",
+            "select": "GroupID",
+            "text": "{GroupID}"
+          },
+          "Filter": {
+            "order": 3,
+            "mode": "MultiSelect",
+            "filter": "GroupID",
+            "text": "GroupID",
+            "sort": "GroupID",
+            "key": "GroupID",
+            "entitySet": "zjblessons_base_Groups",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "SubGroupID",
+          "label": "{i18n>SubGroupID}",
+          "Column": {
+            "order": 4,
+            "sortProperty": "SubGroupID",
+            "visible": true,
+            "type": "text",
+            "select": "SubGroupID",
+            "text": "{SubGroupID}"
+          },
+          "Filter": {
+            "order": 4,
+            "mode": "MultiSelect",
+            "filter": "SubGroupID",
+            "text": "SubGroupID",
+            "sort": "SubGroupID",
+            "key": "SubGroupID",
+            "entitySet": "zjblessons_base_SubGroups",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "Quantity",
+          "label": "{i18n>Quantity}",
+          "Column": {
+            "order": 5,
+            "sortProperty": "Quantity",
+            "visible": true,
+            "type": "number",
+            "select": "Quantity",
+            "number": "{Quantity}",
+            "emphasized": false
+          },
+          "Filter": {
+            "order": 5,
+            "mode": "SearchField",
+            "filter": "Quantity",
+            "text": "Quantity",
+            "sort": "Quantity",
+            "key": "Quantity",
+            "filterKey": "Quantity",
+            "entitySet": "zjblessons_base_Items",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "Price",
+          "label": "{i18n>Price}",
+          "Column": {
+            "order": 6,
+            "sortProperty": "Price",
+            "visible": true,
+            "type": "number",
+            "select": "Price",
+            "number": "{Price}",
+            "emphasized": false
+          },
+          "Filter": {
+            "order": 6,
+            "mode": "SearchField",
+            "filter": "Price",
+            "text": "Price",
+            "sort": "Price",
+            "key": "Price",
+            "filterKey": "Price",
+            "entitySet": "zjblessons_base_Items",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "Created",
+          "label": "{i18n>Created}",
+          "Column": {
+            "order": 7,
+            "sortProperty": "Created",
+            "sortOrder": 1,
+            "sort": "desc",
+            "visible": true,
+            "type": "date",
+            "typeFormat":"medium",
+            "text": "{Created}"
+          },
+          "Filter": {
+            "order": 7,
+            "visible": true,
+            "hidden": false,
+            "mode": "DateField",
+            "datePath": "Created",
+            "dateMode": true,
+            "entitySet": "zjblessons_base_Items",
+            "selectedPeriod": "all",
+            "visiblePeriodButtons": "day, week, month, year, all"
+          }
+        },
+        {
+          "id": "CreatedBy",
+          "label": "{i18n>CreatedBy}",
+          "Column": {
+            "order": 8,
+            "sortProperty": "CreatedByFullName",
+            "visible": true,
+            "type": "avatarAndLink",
+            "select": "CreatedByAvatar, CreatedByFullName",
+          },
+          "Filter": {
+            "order": 8,
+            "mode": "MultiSelect",
+            "filter": "CreatedBy",
+            "text": "CreatedByFullName",
+            "sort": "CreatedByFullName",
+            "image": "CreatedByAvatar",
+            "key": "CreatedBy",
+            "entitySet": "jbcommon_auth_CreatedBy",
+            "visible": true,
+            "hidden": false
+          }
+        },
+        {
+          "id": "Modified",
+          "label": "{i18n>Modified}",
+          "Column": {
+            "order": 9,
+            "sortProperty": "Modified",
+            "sort": "desc",
+            "visible": true,
+            "type": "dateTime",
+            "typeFormat":"medium",
+            "text": "{Modified}"
+          },
+          "Filter": {
+            "order": 9,
+            "visible": true,
+            "hidden": false,
+            "mode": "DateField",
+            "datePath": "Modified",
+            "dateMode": true,
+            "entitySet": "zjblessons_base_Items",
+            "selectedPeriod": "all",
+            "visiblePeriodButtons": "day, week, month, year, all"
+          }
+        },
+        {
+          "id": "ModifiedBy",
+          "label": "{i18n>ModifiedBy}",
+          "Column": {
+            "order": 10,
+            "sortProperty": "ModifiedByFullName",
+            "visible": true,
+            "type": "avatarAndLink",
+            "select": "ModifiedByAvatar, ModifiedByFullName",
+          },
+          "Filter": {
+            "order": 10,
+            "mode": "MultiSelect",
+            "filter": "ModifiedBy",
+            "text": "ModifiedByFullName",
+            "sort": "ModifiedByFullName",
+            "image": "ModifiedByAvatar",
+            "key": "ModifiedBy",
+            "entitySet": "jbcommon_auth_ModifiedBy",
+            "visible": true,
+            "hidden": false
+          }
+        }
+      ]
+    }),
 
         onInit: function () {
             const oViewModel = new JSONModel({
                 sCount: '0'
             });
             this.setModel(oViewModel, "worklistView");
+            
+            this.setModel(jsonAnnotation,"annotation");
         },
         
         onBeforeRendering: function() {
